b98fbc05886eeec3cc898caba221cbf6af208e48
Yan <145013972+Yan8Yan@users.noreply.github.com>
2026-03-11 14:56:46 +0300

    The PlayerController was made

---
+++ KoiKoi/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections.Generic;
+using KoiKoiProject;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    public HandController3D handController;
+
+    [Header("Captured Cards")]
+    public List<Card> kasu = new List<Card>();
+    public List<Card> tanzaku = new List<Card>();
+    public List<Card> tane = new List<Card>();
+    public List<Card> hikari = new List<Card>();
+
+    public int score;
+
+    public void AddCapturedCard(Card card)
+    {
+        switch (card.cardType)
+        {
+            case Card.CardType.kasu:
+                kasu.Add(card);
+                break;
+
+            case Card.CardType.tanzaku:
+                tanzaku.Add(card);
+                break;
+
+            case Card.CardType.tane:
+                tane.Add(card);
+                break;
+
+            case Card.CardType.hikari:
+                hikari.Add(card);
+                break;
+        }
+
+        Debug.Log($"{gameObject.name} captured {card.cardType}");
+    }
+}
